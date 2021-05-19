c50d6d2a8b10b6de6b86641491ef656acecd90d3
kirill-misiuk <kirill.misiuk@intexsoft.by>
2021-05-19 13:43:37 +0300

    init swagger

--- src/common/constants/config.ts
+++ src/common/constants/config.ts
@@ -1,5 +1,11 @@
 export const ENVIRONMENT_VARIABLES = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || '3000',
+  swagger: {
+    title: process.env.SWAGGER_TITLE || 'Booking',
+    description: process.env.SWAGGER_DESCRIPTION || 'Booking rooms API',
+    version: process.env.SWAGGER_VERSION || '1.0',
+    tag: 'booking',
+  },
 };
 export const configuration = () => ENVIRONMENT_VARIABLES;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { ENVIRONMENT_VARIABLES } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(ENVIRONMENT_VARIABLES.swagger.title)
+    .setDescription(ENVIRONMENT_VARIABLES.swagger.description)
+    .setVersion(ENVIRONMENT_VARIABLES.swagger.version)
+    .addTag(ENVIRONMENT_VARIABLES.swagger.tag)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  app.setGlobalPrefix('api');
   await app.listen(ENVIRONMENT_VARIABLES.port);
 }
 bootstrap();
